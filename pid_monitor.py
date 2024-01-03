408af212f0bfe98e174e4dcd93e897ddbbf01a1e
John Thehow <johnthehow@qq.com>
2024-01-03 13:09:37 +0800

    process terminated.

--- pid_monitor.py
+++ pid_monitor.py
@@ -5,41 +5,22 @@ from pathlib import Path
 import datetime
 
 
-def change_monitor(pid,git_repo_path,time_interval,max_time):
+def change_monitor(pid,git_repo_path,time_interval):
 	start_time = time.time()
-	while True:
-		now_time = time.time()
-		if now_time - start_time <= max_time:
-			time.sleep(time_interval)
-			print(f'time elapsed: {int(now_time - start_time)} seconds',end='\x1b\r')
-			ifexist = psutil.pid_exists(pid)
-			if ifexist == False:
-				os.system(f'git -C "{git_repo_path}" pull')
-				time.sleep(10)
-				with open(git_repo_path.joinpath('README.md'),mode='a+',encoding='utf-8') as file:
-					file.write(str(datetime.datetime.now()))
-					file.write('\r\n')
-					file.write(__file__)
-					file.write('\r\n')
-				os.system(f'git -C "{git_repo_path}" add .')
-				os.system(f'git -C "{git_repo_path}" commit -m "process terminated."')
-				os.system(f'git -C "{git_repo_path}" push')
-				break
-			else:
-				pass
-		else:
-			print('max time reached')
-			os.system(f'git -C "{git_repo_path}" pull')
-			time.sleep(10)
-			with open(git_repo_path.joinpath('README.md'),mode='a+',encoding='utf-8') as file:
-				file.write(str(datetime.datetime.now()))
-				file.write('\n')
-				file.write(__file__)
-				file.write('\n')
-			os.system(f'git -C "{git_repo_path}" add .')
-			os.system(f'git -C "{git_repo_path}" commit -m "max time reached."')
-			os.system(f'git -C "{git_repo_path}" push')
-			break
+	while psutil.pid_exists(pid):
+		time.sleep(1)
+		continue
+	else:
+		os.system(f'git -C "{git_repo_path}" pull')
+		time.sleep(10)
+		with open(git_repo_path.joinpath('README.md'),mode='a+',encoding='utf-8') as file:
+			file.write(str(datetime.datetime.now()))
+			file.write('\r\n')
+			file.write(__file__)
+			file.write('\r\n')
+		os.system(f'git -C "{git_repo_path}" add .')
+		os.system(f'git -C "{git_repo_path}" commit -m "process terminated."')
+		os.system(f'git -C "{git_repo_path}" push')
 	return
 
 if __name__ == '__main__':
@@ -49,10 +30,9 @@ if __name__ == '__main__':
 		''')
 	pid = int(input('input pid of the process to be monitored: \n'))
 	git_repo_path = Path(__file__).parent
-	time_interval = 0.1
-	max_time = int(input('Input maximum monitoring time here (in secs): \n'))
+	time_interval = 1
 	
-	change_monitor(pid, git_repo_path, time_interval, max_time)
+	change_monitor(pid, git_repo_path, time_interval)
 
 
 
